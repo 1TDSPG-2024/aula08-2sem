aa8a4efb8266ac3eb766c2a0e5128e3ef8958a73
mateuscastronappe <teteucastro31@gmail.com>
2024-09-05 10:38:14 -0300

    estabelecendo as bases pro inicio dos trabalhos.

--- aula08/src/style/styled.ts
+++ aula08/src/style/styled.ts
@@ -60,3 +60,37 @@ export const MenuLink = styled.a`
   padding: 10px 20px;
   display: block;
 `
+export const MinhaTabela = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin: 20px 0;
+  font-size: 18px;
+  text-align: left;
+
+&  
+thead{
+    background-color: #009879;
+  color: #6f0f0f;
+}
+
+&
+th{
+    padding: 12px 15px;
+  border: 1px solid #dddddd;
+}
+&
+tr{
+    &:nth-child(even) {
+        background-color: #f3f3f3;
+      }
+      &:nth-child(odd) {
+        background-color: #ffffff;
+      }
+}
+&
+td{
+    padding: 12px 15px;
+  border: 1px solid #dddddd;
+}
+
+`
