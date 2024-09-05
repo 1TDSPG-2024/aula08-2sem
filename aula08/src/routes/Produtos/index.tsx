b2f7693a463cb237d4a5240cf0e214a9094b1f6b
Brunosz2712 <brunosouza_27@yahoo.com.br>
2024-09-05 16:53:41 -0300

    Estabelecendo as bases pro inicio dos trabalhos.

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,41 +1,7 @@
 import { Link } from "react-router-dom";
 import { listaProdutos } from "../../listaProdutos";
-import styled from "styled-components";
+import { MinhaTabela } from "../../style/styled";
 
-const MinhaTabela = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  margin: 20px 0;
-  font-size: 18px;
-  text-align: left;
-
-&  
-thead{
-    background-color: #009879;
-  color: #6f0f0f;
-}
-
-&
-th{
-    padding: 12px 15px;
-  border: 1px solid #dddddd;
-}
-&
-tr{
-    &:nth-child(even) {
-        background-color: #f3f3f3;
-      }
-      &:nth-child(odd) {
-        background-color: #ffffff;
-      }
-}
-&
-td{
-    padding: 12px 15px;
-  border: 1px solid #dddddd;
-}
-
-`
 
 export default function Produtos() {
 
