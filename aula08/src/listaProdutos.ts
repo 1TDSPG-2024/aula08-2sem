5d62d261f1af9ebc1bd4d7f2c925db3b7b9d04d8
Felipe-2833 <felipe.elucidator@gmail.com>
2024-09-05 11:28:02 -0300

    Recuperando produto no comp e apresentando no form

--- aula08/src/listaProdutos.ts
+++ aula08/src/listaProdutos.ts
@@ -3,6 +3,8 @@ import { Lista } from "./types";
 //Criar uma lista de produtos eletrônicos com os seguintes dados:
 //id,nome,preco,desc,imagem=path
 
+const imgProdutos = "https://via.placeholder.com/50"
+
 
 export const listaProdutos:Lista[] = [
     {
@@ -10,34 +12,38 @@ export const listaProdutos:Lista[] = [
         nome: "Smartphone",
         preco: 1500.00,
         desc: "Celular com tela de 6 polegadas",
-        imagem: "https://example.com/imagem1.jpg"
+        imagem: imgProdutos
     },
     {
         id: 2,
         nome: "Tablet",
         preco: 800.00,
         desc: "Tablet com tela de 10 polegadas",
-        imagem: "https://example.com/imagem2.jpg"
+        imagem: imgProdutos
     },
     {
         id: 3,
         nome: "Notebook",
         preco: 2500.00,
         desc: "Notebook com processador Intel Core i5",
-        imagem: "https://example.com/imagem3.jpg"
+        imagem: imgProdutos
     },
     {
         id: 4,
         nome: "Impressora",
         preco: 300.00,
         desc: "Impressora de alta qualidade",
-        imagem: "https://example.com/imagem4.jpg"
+        imagem: imgProdutos
     },
     {
         id: 5,
         nome: "Fone de Ouvido",
         preco: 200.00,
         desc: "Fone de ouvido sem fio",
-        imagem: "https://example.com/imagem5.jpg"
+        imagem: imgProdutos
     }
 ];
+
+if(!localStorage.getItem("lista")){
+    localStorage.setItem("lista", JSON.stringify(listaProdutos))
+}
--- aula08/src/routes/EditarProdutos/index.tsx
+++ aula08/src/routes/EditarProdutos/index.tsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { Lista } from "../../types";
+import { useEffect, useState } from "react";
 
 export default function EditarProdutos(){
 
@@ -12,11 +14,41 @@ export default function EditarProdutos(){
       // const{dados} = useParams(), um detalhe aqui é que o useParams() pertence ao react-router e deve ser importado dele
       const {id} = useParams();
 
+      const listaProdutosString = localStorage.getItem("lista") || "[]"
+      const lista:Lista[] =  JSON.parse(listaProdutosString)
+
+      const [produto, setProduto] = useState<Lista>()
+      useEffect(()=>{
+        const produto = lista.find((prod) => prod.id === Number(id))
+      });
+
       return(
       <div>
         <h1>Olá, mundo sou o EditarProdutos!</h1>
         <div>
-          <h2>ID1: {id}</h2>
+          <h2>ID: {id}</h2>
+          <form>
+            <div>
+              <label>Nome:</label>
+              <input type="text" name="nome" value={produto?.nome}  />
+            </div>
+            <div>
+              <label>Preço:</label>
+              <input type="number" name="preco" value={produto?.preco}  />
+            </div>
+            <div>
+              <label>Descrição:</label>
+              <input type="text" name="desc" value={produto?.desc}  />
+            </div>
+            <div>
+              <figure>
+                <img src={produto?.imagem} alt={produto?.desc} />
+              </figure>
+            </div>
+            <div>
+              <button type="button">Editar</button>
+            </div>
+          </form>
         </div>
       </div>
     );
